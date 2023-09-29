d39f93780996ce0b0c508671dfbf0cdca5b986a9
salmarafeen <salmarafeen@gmail.com>
2023-09-28 20:15:51 -0500

    created an opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/TestOpmode.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TestOpmode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
